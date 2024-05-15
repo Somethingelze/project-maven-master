cbd3158a36f9d8897f4edb034feff7741b72096a
Somethingelze <Xxvikingxx@yandex.ru>
2024-05-15 14:54:08 +0300

    first commit

---
+++ src/test/java/StrangeTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class StrangeTest {
+@Disabled
+    @Test
+    public void shouldFail() {
+        fail("By design");
+    }
+}
